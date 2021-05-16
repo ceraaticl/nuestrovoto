45a3834381a4caece6643fc28f3f6671103a5c21
Jose Santamaria <santamaria0323@gmail.com>
2021-05-15 20:27:59 -0400

    CRUD ADMIN

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -5,28 +5,25 @@ const Administrador = require('../models/administrador');
 
 const validarJWT = async (req = request, res = response, next) => {
     const token = req.header('x-token');
-
     if (!token) {
         return res.status(401).json({
-            msg: 'No hay token en la petición.',
+            errors: 'There is no token in the request.',
         });
     }
-
     try {
         const { id } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
         const admin = await Administrador.findByPk(id);
 
         if (!admin) {
             return res.status(401).json({
-                msg: 'Token no válido -  Administrador no existe en BD',
+                errors: 'Invalid token - admin does not exist in the database',
             });
         }
-
         next();
     } catch (error) {
         console.log(error);
         res.status(401).json({
-            msg: 'Token no válido.',
+            errors: 'Invalid token.',
         });
     }
 };
